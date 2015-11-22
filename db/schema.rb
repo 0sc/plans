c3a70913981a16ef735ef6834e08e561412b0d37
Osmond Oranagwa <vagrant@precise32.(none)>
2015-11-22 20:21:42 +0000

    Remove root node from json response

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119200851) do
+ActiveRecord::Schema.define(version: 20151122194921) do
 
   create_table "lists", force: :cascade do |t|
     t.string   "type"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20151119200851) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.boolean  "active"
   end
 
 end
